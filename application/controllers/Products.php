d567a84a6b66e4807bf7cdb46f56a00a893d10cd
isouza <isouza@gattecnologia.com.br>
2025-06-08 19:25:16 -0300

    feat: Cria controller de produtos

---
+++ application/controllers/Products.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Products extends CI_Controller
+{
+  private ProductService $productService;
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->library('session');
+    $this->load->helper(['url']);
+
+    $this->load->library('ProductService');
+    $this->productService = new ProductService();
+  }
+
+  public function create()
+  {
+    if($this->input->post('id')){
+      $productId = (int)$this->input->post('id');
+      $product = $this->productService->getProductById($productId);
+
+      if (!$product) {
+        show_404();
+      }
+      
+      $this->productService->updateProduct($productId, [
+        'name' => $this->input->post('name'),
+        'price' => (float)$this->input->post('price'),
+        'stock' => (int)$this->input->post('stock', true) ?: 0,
+      ]);
+      return;
+    }
+
+    $this->productService->createProduct([
+      'name' => $this->input->post('name'),
+      'price' => (float)$this->input->post('price'),
+      'stock' => (int)$this->input->post('stock', true) ?: 0,
+    ]);
+  }
+}
